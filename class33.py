914498a2ae258b2ec3ee6be5c8018c61f0806ac9
cyumair <47939238+cyumair@users.noreply.github.com>
2019-12-06 17:38:24 +0500

    Update class33.py

--- class33.py
+++ class33.py
@@ -1,24 +1,21 @@
-lcm = []
+lcm = []  
 def LCM(numbers):
-    maximum = max(numbers)
+    maximum = max(numbers)   
     highestpossiblemultiple = 1
-    for i in numbers:
-        highestpossiblemultiple = highestpossiblemultiple * i
-    
-    while maximum <= highestpossiblemultiple:
-        mults = 0
-        for i in numbers:
+    for i in numbers:                                                # calculating what can be highest possible factor
+        highestpossiblemultiple = highestpossiblemultiple * i        # for example higest possible factor for 3 and 4 is 3*4 = 12
+                                                                     
+    while maximum <= highestpossiblemultiple:                  # if number a has no lcm then highest possible factor is its factor
+        mults = 0                                              # so end the loop on that highest possible factor  
+        for i in numbers:                                           
             if maximum % i == 0:
-                mults += 1
-        if mults == len(numbers):
+                mults += 1    #this will keep a check if all (not one) the numbers in the list are factor of maximum(lcm) 
+        if mults == len(numbers):  # if the number(maximum) is divided by all the numbers of list
             lcm.append(maximum)
-            break
+            break                 #if this happens we found our lcm break out of loop
         else:
-            maximum += 1
-        
-            
-            
-                
+            maximum += 1         #else try some other number greater than maximum, that number might be lcm
+                   
 numbers = []
 check = 1
 while check == 1:
@@ -27,7 +24,7 @@ while check == 1:
     check = int(input('press 1 to add more numbers '))
 
 LCM(numbers)
-if len(lcm) == 0:
+if len(lcm) == 0:   
     print('no lcm')
 else:
     print('Lcm is ' , min(lcm))
